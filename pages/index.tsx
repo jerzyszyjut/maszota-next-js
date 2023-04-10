f960bd412f3ae695bba5412b5ff9ff17f6882026
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2023-04-10 16:22:11 +0200

    Add basic meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,17 @@ const Home = () => {
             <Head>
                 <title>{t('common:site-title')}</title>
                 <meta name="description" content={t('common:site-description') ?? ''} />
+                <meta name="keywords" content={t('common:site-keywords') ?? ''} />
+                <meta name="og:title" content={t('common:site-title') ?? ''} />
+                <meta name="og:description" content={t('common:site-description') ?? ''} />
+                <meta name="og:image" content={t('common:site-image') ?? ''} />
+                <meta name="og:url" content={t('common:site-url') ?? ''} />
+                <meta name="og:site_name" content={t('common:site-title') ?? ''} />
+                <meta name="og:type" content="website" />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:title" content={t('common:site-title') ?? ''} />
+                <meta name="twitter:description" content={t('common:site-description') ?? ''} />
+                <meta name="twitter:image" content={t('common:site-image') ?? ''} />
             </Head>
             <main>
                 <NavigationBar />
