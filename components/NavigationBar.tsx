148cbc1f71ff4ac5df90f5613a86707c81669970
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2023-04-10 16:58:47 +0200

    Add changing locale button

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
 import { useTranslation } from 'next-i18next'
 import { useState } from "react";
 import Logo from "./Logo"
 import HamburgerButton from "./HamburgerButton"
+import FlagPL from "../assets/flags/pl.png"
+import FlagEN from "../assets/flags/en.png"
 import style from './NavigationBar.module.scss'
 
 const links = [
@@ -28,6 +32,14 @@ const links = [
 const NavigationBar = () => {
     const { t } = useTranslation(['navigation-bar']);
     const [isOpen, setIsOpen] = useState(false);
+    const router = useRouter();
+
+    const changeLanguage = (newLocale: string) => {
+        const { pathname, asPath, query } = router;
+        router.push({ pathname, query }, asPath, { locale: newLocale });
+    }
+
+    const changeTo = router.locale === 'en' ? 'pl' : 'en';
 
     return (
         <nav className="w-full flex justify-center items-center flex-col lg:flex-row p-4 bg-white">
@@ -38,15 +50,36 @@ const NavigationBar = () => {
                     isActivated={isOpen}
                 />
             </div>
-            <ul className={`flex justify-around items-center flex-col lg:flex-row px-0 lg:px-4 transition-all ${!isOpen ? style.hidden : ''}`}>
-                {links.map(({ href, label }) => (
-                    <li key={`${href}${label}`}>
-                        <Link onClick={() => setIsOpen((previousState) => !previousState)} href={href} className="w-auto lg:w-40 mx-2 py-4 text-xl uppercase flex justify-center items-center hover:text-ma-s transition-all lg:border-b-2 lg:border-ma-s">
-                            {t(label)}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <div className={`flex justify-around items-center flex-col lg:flex-row px-0 lg:px-4 transition-all ${!isOpen ? style.hidden : ''}`}>
+                <ul className={`flex justify-around items-center flex-col lg:flex-row px-0 lg:px-4 transition-all ${!isOpen ? style.hidden : ''}`}>
+                    {links.map(({ href, label }) => (
+                        <li key={`${href}${label}`}>
+                            <Link onClick={() => setIsOpen((previousState) => !previousState)} href={href} className="w-auto lg:w-40 mx-2 py-4 text-xl uppercase flex justify-center items-center hover:text-ma-s transition-all lg:border-b-2 lg:border-ma-s">
+                                {t(label)}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                {changeTo === 'pl' ?
+                    <Image
+                        src={FlagPL}
+                        alt="Polish flag"
+                        width={30}
+                        height={30}
+                        onClick={() => changeLanguage(changeTo)}
+                        className="cursor-pointer"
+                    />
+                    :
+                    <Image
+                        src={FlagEN}
+                        alt="English flag"
+                        width={30}
+                        height={30}
+                        onClick={() => changeLanguage(changeTo)}
+                        className="cursor-pointer"
+                    />
+                }
+            </div>
         </nav>
     )
 }
