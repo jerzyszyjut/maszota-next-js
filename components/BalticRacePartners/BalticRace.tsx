c7a8ee4387b236941ee47428e72e177a705b7724
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2024-07-20 12:52:22 +0200

    Add translations to partners

--- components/BalticRacePartners/BalticRace.tsx
+++ components/BalticRacePartners/BalticRace.tsx
@@ -1,7 +1,10 @@
 import { CldImage } from "next-cloudinary";
 import styles from "./BalticRace.module.scss";
+import { useTranslation } from "next-i18next";
 
 const BalticRace = () => {
+  const { t } = useTranslation(["partners"]);
+
   return (
     <div
       className={styles.background}
@@ -11,7 +14,7 @@ const BalticRace = () => {
       <div className={styles.background_polygon_2} />
       <div className="flex items-center justify-center flex-col py-4 relative">
         <h1 className="text-white text-4xl font-bold text-center p-4 uppercase">
-          Partnerzy
+          {t("baltic-race")}
         </h1>
         <CldImage
           key={"logo_baltic_race"}
