b4e4363b2b81c0181a20ea4a1d704f0a0a552d02
jerzyszyjut <jerzy.szyjut@outlook.com>
2025-08-27 14:27:39 +0200

    Update baltic race logo

--- components/BalticRacePartners/BalticRace.tsx
+++ components/BalticRacePartners/BalticRace.tsx
@@ -17,10 +17,10 @@ const BalticRace = () => {
           {t("baltic-race")}
         </h1>
         <CldImage
-          key={"logo_baltic_race"}
+          key={"logo_two_udssc5"}
           width={400}
           height={200}
-          src={"logo_baltic_race"}
+          src={"logo_two_udssc5"}
           alt={"Baltic race logo"}
         />
       </div>
